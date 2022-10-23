fa5175cbb6f52f7b64eed279f8321ddc365d1644
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-10-23 17:14:10 +0200

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Patryk Jędrzejczak
+// author - Patryk Jędrzejczak
 
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -6,8 +6,12 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
+// Type of the database of the key-value pairs.
 type Db = Arc<Mutex<HashMap<String, String>>>;
 
+// Maximum size of request that is allowed by server. If server receives a longer
+// message without a single prefix that is a correct request, it closes the
+// connection with the sender.
 const MAX_MESSAGE_LENGTH: usize = 10000;
 
 #[tokio::main]
@@ -29,48 +33,38 @@ async fn main() {
 
 async fn handle_connection(mut data: TaskData) {
     let mut buf = vec![0; MAX_MESSAGE_LENGTH];
-    let mut request = String::new();
+    let mut request = String::new(); // Request fragment read so far.
 
     loop {
-        match data.socket.read(&mut buf).await {
-            Ok(0) => return,
-            Ok(read_num) => {
-                for i in 0..read_num {
-                    request.push(buf[i] as char);
-                }
+        if let Ok(read_num) = data.socket.read(&mut buf).await {
+            buf[0..read_num].iter().for_each(|byte| request.push(*byte as char));
 
-                if request.len() > MAX_MESSAGE_LENGTH {
-                    return
-                }
-
-                if let Err(_) = process_request(&mut request, &mut data).await {
-                    return
-                }
+            if process_request(&mut request, &mut data).await.is_err() {
+                return
             }
-            Err(_) => return
+        } else { // The case where client closed the connection or socket.read failed.
+            return
         }
     }
 }
 
-async fn process_request(request: &mut String, data: &mut TaskData) -> Result<(), RequestError> {
-    let is_store = is_store_request(request)?;
-    if is_store {
-        if process_store_request(request, data).await.is_err() {
-            return Err(RequestError)
-        }
+async fn process_request(request: &mut String, data: &mut TaskData) -> Result<(), TaskError> {
+    if is_store_request(request)? {
+        process_store_request(request, data).await?;
     }
     
-    let is_load = is_load_request(request)?;
-    if is_load {
-        if process_load_request(request, data).await.is_err() {
-            return Err(RequestError)
-        }
+    if is_load_request(request)? {
+        process_load_request(request, data).await?;
+    }
+
+    if request.len() > MAX_MESSAGE_LENGTH {
+        return Err(TaskError)
     }
 
     Ok(())
 }
 
-async fn process_store_request(request: &mut String, data: &mut TaskData) -> Result<(), RequestError> {
+async fn process_store_request(request: &mut String, data: &mut TaskData) -> Result<(), TaskError> {
     let (key, value, rest) = split_store_request(request);
     *request = rest;
 
@@ -78,15 +72,15 @@ async fn process_store_request(request: &mut String, data: &mut TaskData) -> Res
         Ok(mut db) => {
             db.insert(key, value);
         },
-        Err(_) => return Err(RequestError)
+        Err(_) => return Err(TaskError)
     }
 
-    send_done_response(data).await?;
+    send_done_response(&mut data.socket).await?;
 
     Ok(())
 }
 
-async fn process_load_request(request: &mut String, data: &mut TaskData) -> Result<(), RequestError> {
+async fn process_load_request(request: &mut String, data: &mut TaskData) -> Result<(), TaskError> {
     let (key, rest) = split_load_request(request);
     *request = rest;
 
@@ -97,28 +91,38 @@ async fn process_load_request(request: &mut String, data: &mut TaskData) -> Resu
                 value = val.clone();
             }
         },
-        Err(_) => return Err(RequestError)
+        Err(_) => return Err(TaskError)
     }
 
-    send_found_response(data, value).await?;
+    if value == "not_found" {
+        send_not_found_response(&mut data.socket).await?;
+    } else {
+        send_found_response(&mut data.socket, value).await?;
+    }
 
     Ok(())
 }
 
-fn is_store_request(request: &str) -> Result<bool, RequestError> {
+// Returns true if there exists a prefix of a request parameter
+// that is a correct STORE request.
+fn is_store_request(request: &str) -> Result<bool, TaskError> {
     match Regex::new(r"^STORE\$[a-z]*\$[a-z]*\$") {
         Ok(store_regex) => Ok(store_regex.is_match(request)),
-        Err(_) => Err(RequestError)
+        Err(_) => Err(TaskError)
     }
 }
 
-fn is_load_request(request: &str) -> Result<bool, RequestError> {
+// Returns true if there exists a prefix of a request parameter
+// that is a correct LOAD request.
+fn is_load_request(request: &str) -> Result<bool, TaskError> {
     match Regex::new(r"^LOAD\$[a-z]*\$") {
         Ok(store_regex) => Ok(store_regex.is_match(request)),
-        Err(_) => Err(RequestError)
+        Err(_) => Err(TaskError)
     }
 }
 
+// Splits a string with a prefix that is a correct STORE request
+// from STORE$key$value$rest to (key, value, rest).
 fn split_store_request(request: &str) -> (String, String, String) {
     let dollars: Vec<usize> = request.match_indices('$').map(|(pos, _)| pos).collect();
     let key = request[dollars[0] + 1..dollars[1]].to_string();
@@ -127,6 +131,8 @@ fn split_store_request(request: &str) -> (String, String, String) {
     (key, value, rest)
 }
 
+// Splits a string with a prefix that is a correct LOAD request
+// from LOAD$key$rest to (key, rest).
 fn split_load_request(request: &str) -> (String, String) {
     let dollars: Vec<usize> = request.match_indices('$').map(|(pos, _)| pos).collect();
     let key = request[dollars[0] + 1..dollars[1]].to_string();
@@ -134,31 +140,34 @@ fn split_load_request(request: &str) -> (String, String) {
     (key, rest)
 }
 
-async fn send_done_response(data: &mut TaskData) -> Result<(), RequestError> {
-    if data.socket.write("DONE".as_bytes()).await.is_err() {
-        return Err(RequestError)
+async fn send_done_response(socket: &mut TcpStream) -> Result<(), TaskError> {
+    match socket.write("DONE$".as_bytes()).await {
+        Ok(_) => Ok(()),
+        Err(_) => Err(TaskError)
     }
-
-    Ok(())
 }
 
-async fn send_found_response(data: &mut TaskData, value: String) -> Result<(), RequestError> {
-    if value == "not_found" {
-        if data.socket.write("NOTFOUND$".as_bytes()).await.is_err() {
-            return Err(RequestError)
-        }
+async fn send_found_response(socket: &mut TcpStream, value: String) -> Result<(), TaskError> {
+    match socket.write(format!("FOUND${value}$").as_bytes()).await {
+        Ok(_) => Ok(()),
+        Err(_) => Err(TaskError)
     }
-    else {
-        if data.socket.write(format!("FOUND${value}$").as_bytes()).await.is_err() {
-            return Err(RequestError)
-        }
-    }
-
-    Ok(())
 }
 
-struct RequestError;
+async fn send_not_found_response(socket: &mut TcpStream) -> Result<(), TaskError> {
+    match socket.write("NOTFOUND$".as_bytes()).await {
+        Ok(_) => Ok(()),
+        Err(_) => Err(TaskError)
+    }
+}
 
+// Error returned when something goes wrong during a task's work.
+// We do not care what really happened because in every case we just
+// finish the task and close the connection with the client.
+struct TaskError;
+
+// Struct keeping data of a single task. Its only purpose is
+// simplifying definitions of some functions.
 struct TaskData {
     socket: TcpStream,
     db: Db
